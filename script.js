215fa56d6a556a172edc78ab7d6f837d88e8b2e9
Karto4an <1sasha.ignatenko1@gmail.com>
2021-10-13 19:38:51 +0300

    Select/deselect

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-var qwerty
+var selected_color;
+
+const red_button = document.querySelectorAll('.color-picker-red')[0];
+const green_button = document.querySelectorAll('.color-picker-green')[0];
+const blue_button = document.querySelectorAll('.color-picker-blue')[0];
 
 document.querySelectorAll('.hexagon').forEach(item => {
     item.addEventListener('click', event => {
         console.log(event.target);
-        qwerty = event.target;
-        qwerty.style.animation = "fadeOut 1s both";
+        item.style.animation = "fadeOut 1s both";
     });
 });
+
+// border: solid gold 5px;
+
+
+red_button.addEventListener('click', event => {
+    console.log(event.target);
+    uncheck_colors();
+    red_button.classList.add('checked');
+});
+
+
+green_button.addEventListener('click', event => {
+    console.log(event.target);
+    uncheck_colors();
+    green_button.classList.add('checked');
+});
+
+
+blue_button.addEventListener('click', event => {
+    console.log(event.target);
+    uncheck_colors();
+    blue_button.classList.add('checked');
+});
+
+
+function uncheck_colors() {
+    if (red_button.classList.contains('checked')){
+        red_button.classList.remove('checked');
+    }
+    if (green_button.classList.contains('checked')){
+        green_button.classList.remove('checked');
+    }
+    if (blue_button.classList.contains('checked')){
+        blue_button.classList.remove('checked');
+    }
+}
