e2e3233427a6bed873b49cce9a4da1028069e2e9
Karto4an <1sasha.ignatenko1@gmail.com>
2021-10-15 21:41:53 +0300

    Show mixed color

--- script.js
+++ script.js
@@ -55,18 +55,79 @@ document.querySelectorAll('.hexagon').forEach(item => {
 						}
 					});
 				});
-			selected = [];
-			mix_num = 0;
-			to_mix = [];
-			i = 0;
+				console.log(color_1);
+				selected = [];
+				mix_num = 0;
+				to_mix = [];
+				i = 0;
 
-			if (color_1 != undefined && color_2 != undefined){
-				console.log(color_1, color_2)
-			}
+				color_1 = color_1.replace(/[^0-9\,]+/g, "");
+				color_2 = color_2.replace(/[^0-9\,]+/g, "");
+
+				var color_1_arr = color_1.split(",");
+				var color_2_arr = color_2.split(",");
+				//console.log(typeof(color_1_arr));
+
+				var c_1_r = color_1_arr[0];
+				var c_1_g = color_1_arr[1];
+				var c_1_b = color_1_arr[2];
+
+				var c_2_r = color_2_arr[0];
+				var c_2_g = color_2_arr[1];
+				var c_2_b = color_2_arr[2];
+
+				var n_c_r = Math.ceil(c_1_r - (c_1_r - c_2_r)/2);
+				var n_c_g = Math.ceil(c_1_g - (c_1_g - c_2_g)/2);
+				var n_c_b = Math.ceil(c_1_b - (c_1_b - c_2_b)/2);
+				console.log(n_c_r, n_c_g, n_c_b);
+
+				document.querySelectorAll('.color-picker-confirm svg')[0].style.backgroundColor = `rgb(${n_c_r}, ${n_c_g}, ${n_c_b})`;
 			}
 		}
 	});
 });
+
+function split_rgb(color_1, color_2) {
+	var color_1_r;
+	var color_1_g;
+	var color_1_b;
+
+	if (color_1 != undefined && color_2 != undefined){
+	color_1 = color_1.replace(/[^0-9\,]+/g, "");
+	color_2 = color_2.replace(/[^0-9\,]+/g, "");
+
+	var color_1_arr = color_1.split(",");
+	var color_2_arr = color_2.split(",");
+	console.log(color_2_arr, color_1_arr);
+
+	var i = 0;
+	color_1_arr.forEach(element => {
+		if (i == 0) {
+			color_1_r = element;
+		}
+		if (i == 1) {
+			color_1_g = element;
+		}
+		if (i == 2) {
+			color_1_b = element;
+		}
+	});
+	color_2_arr.forEach(element => {
+		// if (i == 0) {
+		// 	console.log(element);
+		// }
+		// if (i == 1) {
+		// 	console.log(element);
+		// }
+		// if (i == 2) {
+		// 	console.log(element);
+		// }
+	});
+}
+i = 0;
+console.log(color_1_r, color_1_g, color_1_b);
+return color_1_r, color_1_g, color_1_b;
+}
 
 // border: solid gold 5px;
 
